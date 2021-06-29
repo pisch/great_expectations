fd66e6d717eaf5040d2064d0be6d8d1f84b1d197
Shinnnyshinshin <will@superconductive.com>
2021-06-29 11:26:08 -0700

    Update mssql_yaml_example.py

--- tests/integration/docusaurus/connecting_to_your_data/database/mssql_yaml_example.py
+++ tests/integration/docusaurus/connecting_to_your_data/database/mssql_yaml_example.py
@@ -39,23 +39,23 @@ datasource_yaml = datasource_yaml.replace(
 context.test_yaml_config(datasource_yaml)
 
 context.add_datasource(**yaml.load(datasource_yaml))
-#
-# # Here is a RuntimeBatchRequest using a query
-# batch_request = RuntimeBatchRequest(
-#     datasource_name="my_mssql_datasource",
-#     data_connector_name="default_runtime_data_connector_name",
-#     data_asset_name="default_name",  # this can be anything that identifies this data
-#     #runtime_parameters={"query": "select top 10 * from taxi_data"},
-#     runtime_parameters={"query": "select * from taxi_data"},
-#     batch_identifiers={"default_identifier_name": "something_something"},
-# )
-# context.create_expectation_suite(
-#     expectation_suite_name="test_suite", overwrite_existing=True
-# )
-# validator = context.get_validator(
-#     batch_request=batch_request, expectation_suite_name="test_suite"
-# )
-# print(validator.head())
+
+# Here is a RuntimeBatchRequest using a query
+batch_request = RuntimeBatchRequest(
+    datasource_name="my_mssql_datasource",
+    data_connector_name="default_runtime_data_connector_name",
+    data_asset_name="default_name",  # this can be anything that identifies this data
+    runtime_parameters={"query": "select top 10 * from taxi_data"},
+    #runtime_parameters={"query": "select * from taxi_data"},
+    batch_identifiers={"default_identifier_name": "something_something"},
+)
+context.create_expectation_suite(
+    expectation_suite_name="test_suite", overwrite_existing=True
+)
+validator = context.get_validator(
+    batch_request=batch_request, expectation_suite_name="test_suite"
+)
+print(validator.head())
 
 # NOTE: The following code is only for testing and can be ignored by users.
 # assert isinstance(validator, ge.validator.validator.Validator)
